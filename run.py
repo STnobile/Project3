9ded97c69ede3276af4029111a9ac0d42d767250
STnobile <stnobile0@gmail.com>
2023-02-14 11:29:46 +0000

    Add the creds file, one more time, because the heroku doesn't work

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive",
 ]
 
-CREDS = Credentials.from_service_account_file("wh-users.json")
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("usersdata")
